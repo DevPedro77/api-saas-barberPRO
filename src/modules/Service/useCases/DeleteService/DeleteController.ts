fd4ebe6985c977dd7753ef46235decd0b17f57d9
Pedro Castro <pcastro9876@gmail.com>
2025-10-03 17:44:43 -0300

    docs: criando o setup do swagger

--- src/modules/Service/useCases/DeleteService/DeleteController.ts
+++ src/modules/Service/useCases/DeleteService/DeleteController.ts
@@ -1,6 +1,25 @@
 import { Request, Response } from 'express';
 import DeleteService from '../DeleteService/DeleteService';
 
+/**
+ * @swagger
+ * /services/{id}:
+ *   delete:
+ *     summary: Remove um serviço pelo ID
+ *     tags: [Serviços]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID do serviço
+ *     responses:
+ *       200:
+ *         description: Serviço removido com sucesso
+ *       404:
+ *         description: Serviço não encontrado
+ */
 class DeleteController {
   async delete(req: Request, res: Response) {
     const { id } = req.params;
